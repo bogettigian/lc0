ea8b73896325bfbc0021a320e9714624400e83da
BogettiGian <gianbogetti7@hotmail.com>
2023-05-30 16:04:22 -0300

    Update windows build

--- build-cuda.cmd
+++ build-cuda.cmd
@@ -1,16 +1,18 @@
 rd /s build
 
-rem set MSBuild="C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
-set MSBuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
-rem call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
-meson.py build --backend vs2017 --buildtype release ^
--Dcudnn_libdirs="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.0\lib\x64","C:\dev\cuDNN\cuda\lib\x64" ^
--Dcudnn_include="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.0\include","C:\dev\cuDNN\cuda\include" ^
--Ddefault_library=static
+set MSBuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" amd64
+
+meson --backend vs2017 --buildtype release ^
+-Dispc_native_only=false ^
+-Dpopcnt=false ^
+-Dcudnn=true ^
+-Dopencl=false ^
+-Dcudnn_libdirs="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2\lib\x64" ^
+-Dcudnn_include="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2\include" ^
+-Ddefault_library=static build/
 
 pause
-
 
 cd build
 
@@ -19,4 +21,3 @@ cd build
 /p:Platform=x64 ^
 /p:PreferredToolArchitecture=x64 lc0.sln ^
 /filelogger
-
