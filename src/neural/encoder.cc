25f841b1be565a5d489c787a5e71527b55863acf
bogettigian <gianbogetti7@hotmail.com>
2024-03-19 01:14:28 -0300

    Update elo log

--- src/neural/encoder.cc
+++ src/neural/encoder.cc
@@ -56,7 +56,7 @@ InputPlanes EncodePositionForNN(const PositionHistory& history,
     std::cerr << "Working" << "\n";
     if (std::getenv("elo") == NULL) std::cerr << "READING ELO NULL" << "\n";
 
-    std::cerr << "READING ELO: " std::getenv("elo") << "\n";
+    std::cerr << "READING ELO: " << std::getenv("elo") << "\n";
     if (std::getenv("elo")) {
       result[kAuxPlaneBase + 6].Fill(
           strtof(std::getenv("elo"), (char**)NULL) / 400);
